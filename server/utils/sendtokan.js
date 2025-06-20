c4bc5b0219836a4fbdd7b9c7ae277805a8b46afa
kishore0122 <kishorepanuganti278@gmail.com>
2025-06-20 15:02:52 +0530

    bjdhgo

--- server/utils/sendtokan.js
+++ server/utils/sendtokan.js
@@ -1,14 +1,16 @@
-export const sendtokan = (user,statuscode,message, res) => {
+export const sendtokan = (user, statuscode, message, res) => {
     const token = user.generatetoken();
-    res.status(statuscode).cookie("token", token,{
-        expires: new Date(Date.now() +process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
-        httpOnly:true,
-        secure:true,
-        sameSite: "None"
+    res.status(statuscode).cookie("token", token, {
+        expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+        httpOnly: true,
+        secure: true, // Must be true for HTTPS
+        sameSite: "None", // Must be 'None' for cross-site cookies
+        // domain: ".kishore-library.netlify.app" // Uncomment and set if needed for subdomains
     })
     .json({
-        success:true,
+        success: true,
         message,
         user,
-    })
+        token, // Optionally return token for debugging
+    });
 }
