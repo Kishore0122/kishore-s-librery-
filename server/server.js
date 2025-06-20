036b20255c7a37c2159e125c7f4564263d495498
kishore0122 <kishorepanuganti278@gmail.com>
2025-06-20 12:27:02 +0530

    hlo

--- server/server.js
+++ server/server.js
@@ -10,9 +10,6 @@ cloudinary.config({
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
 
 // Start server
 const startServer = async () => {
